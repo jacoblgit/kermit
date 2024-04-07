4537f8c3f04022767e21cb287f68af3246432c39
Jacob Lessing <jacob.lessing@yale.edu>
2024-04-07 12:48:08 -0400

    cleaned up setup

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
 
+void setUpPins();
+void setUpMotors();
+
 // Motor Connections
 const int motorEnPin = 2;
 // Motor 1 
@@ -11,16 +14,17 @@ const int mot1_stepPin = 3;
 const int mot1_dirPin = 4; 
 // Motor 2
 const int mot2_stepPin = 5;
-const int mot2_dirPin = 6; 
+const int mot2_dirPin = 6;
+
+// Joystick Connections
+// const int joy_y;
 
 AccelStepper mot1(AccelStepper::DRIVER, mot1_stepPin, mot1_dirPin);
 AccelStepper mot2(AccelStepper::DRIVER, mot2_stepPin, mot2_dirPin);
 
 void setup() {
-  pinMode(motorEnPin, OUTPUT);
-  digitalWrite(motorEnPin, LOW);
-  mot1.setMaxSpeed(1000);
-  mot2.setMaxSpeed(1000);
+  setUpPins();
+  setUpMotors();
   mot1.setSpeed(200);
   mot2.setSpeed(200);
 }
@@ -28,4 +32,16 @@ void setup() {
 void loop() {
   mot1.runSpeed();
   mot2.runSpeed();
+}
+
+void setUpPins() {
+  // motors
+  pinMode(motorEnPin, OUTPUT);
+  
+}
+
+void setUpMotors() {
+  digitalWrite(motorEnPin, LOW);
+  mot1.setMaxSpeed(1000);
+  mot2.setMaxSpeed(1000);
 }
